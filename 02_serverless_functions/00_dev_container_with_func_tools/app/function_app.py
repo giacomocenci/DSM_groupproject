e560f093c6bd4c4d3c375dffb2efebc1a9c9536b
jan-source <jan.mammen@googlemail.com>
2024-12-12 08:37:22 +0100

    - added excel container for model training

--- 02_serverless_functions/00_dev_container_with_func_tools/app/function_app.py
+++ 02_serverless_functions/00_dev_container_with_func_tools/app/function_app.py
@@ -1,6 +1,4 @@
 import azure.functions as func
-import datetime
-import json
 import logging
 
 app = func.FunctionApp()
@@ -10,13 +8,7 @@ def test(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+   
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
@@ -25,3 +17,4 @@ def test(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+    
