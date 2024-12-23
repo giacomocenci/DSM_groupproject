841e2b115edf53aa58829f2d76b835901faeedd1
giacomocenci <146782390+giacomocenci@users.noreply.github.com>
2024-12-23 12:40:10 +0100

    Delete 03_user_interfaces/01_js_app directory

--- 03_user_interfaces/01_js_app/src/main.js
+++
@@ -1,6 +0,0 @@
-
-
-import { testFunction, blockingFunction } from "./exampleFunctions.js";
-
-document.getElementById('runButton').addEventListener('click', testFunction);
-document.getElementById('runButtonBlocking').addEventListener('click', blockingFunction);
